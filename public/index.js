094b430602a6c7af18704f83af6c3c73c4e6d2f0
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-06-30 13:46:16 -0500

    first commit with search

--- public/index.js
+++ public/index.js
@@ -1,1 +1,26 @@
-//stuff
+
+const prods = document.getElementById("prods");
+//const userVal =  document.getElementById("myInput").value
+
+
+products.map(arr => 
+    prods.innerHTML += `<div><img src="${arr.imgUrl}"/><br>${arr.name}<br>${arr.description}</div><br>`
+   )
+
+
+function findProd(){
+    const userVal = document.getElementById("myInput").value.toLowerCase();
+    
+for (let i = 0; i < products.length; i++){
+
+    const prodQuery = products[i].name.toLowerCase();
+
+ if(userVal == prodQuery){
+ document.getElementById("message").innerHTML = "Product id " + `${products[i].id}` + " matches your description"
+} else {
+    console.log(userVal, prodQuery)
+}
+  }
+}
+  
+
